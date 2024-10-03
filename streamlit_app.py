6ab084559ad1dc77078add292d5e7267de54b93b
Esben922 <tr67nh@student.aau.dk>
2024-10-03 22:16:01 +0200

    Small fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 pg = st.navigation([st.Page("EDA.py"), st.Page("Loan Amount Prediction.py")])
 pg.run()
+
+
+# Footer
+st.markdown("---")
+st.markdown("Developed by Camilla Louise Jensen, Esben Graahede, Imran Talukder, Piyal Dey, & Samil Demiroglu")
